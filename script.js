8cbbe233a7fac6a248f1338d38cc2f9ad0657718
Dij1022 <90654140+Divija1967@users.noreply.github.com>
2022-09-28 23:39:58 -0400

    Added images to array of pokemon

--- script.js
+++ script.js
@@ -1,32 +1,32 @@
 
 // array of pokemon objects: includes name, number, and descriptions of Pokémon
 let pokemon = [
-    {name: "Bulbasaur", number: 1, description: " a Grass/Poison Type Pokémon with Clear/Cloudy Weather and a Rarity of 4. It is in its First Evolutionary Stage with a Base Attack Level of 118\n\n"},
-    {name: "Ivysaur", number: 2, description: " a Grass/Poison Type Pokémon with Clear/Cloudy Weather and a Rarity of 5. It is in its Second Evolutionary Stage with a Base Attack Level of 151\n\n" },
-    {name: "Venasaur", number: 3, description: " a Grass/Poison Type Pokémon with Clear/Cloudy Weather and a Rarity of 6. It is in its Third Evolutionary Stage with a Base Attack Level of 198\n\n"},
-    {name: "Mega-Venasaur", number: 3.1, description: " the Mega version of Venasaur Type Pokémon with extra strength and rarity\n\n"},
-    {name: "Charmander", number: 4, description: " a Fire Type Pokémon with Clear Weather and a Rarity of 4. It is in its First Evolutionary Stage Type Pokémon with a Base Attack Level of 116\n\n"},
-    {name: "Charmeleon", number: 5, description: " a Fire Type Pokémon with Clear Weather and a Rarity of 5. It is in its Second Evolutionary Stage Type Pokémon with a Base Attack Level of 158\n\n"},
-    {name: "Charizard", number: 6, description: " a Fire/Flying Type Pokémon with Clear/Windy Weather and a Rarity of 6. It is in its Third Evolutionary Stage with a Base Attack Level of 223\n\n"},
-    {name: "Mega-X-Charizard", number: 6.1, description: " a Mega version of Charizard Type Pokémon with extra strength and rarity\n\n"},
-    {name: "Mega-Y-Charizard", number: 6.2, description: " a Mega version of Charizard Type Pokémon with extra powers and rarity\n\n"},
-    {name: "Squirtle", number: 7, description: " a Water Type Pokémon with Rain Weather and a Rarity of 4. It is in its First Evolutionary Stage with a Base Attack Level of 94\n\n"},
-    {name: "Wartortle", number: 8, description: " a Water Type Pokémon with Rain Weather and a Rarity of 5. It is in its Second Evolutionary Stage with a Base Attack Level of 126\n\n"},
-    {name: "Blastoise", number: 9, description: " a Water Type Pokémon with Rain Weather and a Rarity of 6. It is in its Third Evolutionary Stage with a Base Attack Level of 171\n\n"},
-    {name: "Mega-Blastoise", number: 9.1, description: " a Mega version of Blastoise with extra strength and rarity\n\n"},
-    {name: "Caterpie", number: 10, description: " a Bug Type Pokémon with Rain Weather and a Rarity of 1. It is in its First Evolutionary Stage with a Base Attack Level of 55\n\n"},
-    {name: "Metapod", number: 11, description: " a Bug Type Pokémon with Rain Weather and a Rarity of 3. It is in its Second Evolutionary Stage with a Base Attack Level of 45\n\n"},
-    {name: "Butterfree", number: 12, description: " a Bug/Flying Type Pokémon with Rain/Windy Weather and a Rarity of 4. It is in its Third Evolutionary Stage with a Base Attack Level of 167\n\n"},
-    {name: "Weedle", number: 13, description: " a Bug/Poison Type Pokémon with Rain/Cloudy Weather and a Rarity of 1. It is in its First Evolutionary Stage with a Base Attack Level of 63\n\n"},
-    {name: "Kakuna", number: 14, description: " a Bug/Poison Type Pokémon with Rain/Cloudy Weather and a Rarity of 3. It is in its Second Evolutionary Stage with a Base Attack Level of 46\n\n"},
-    {name: "Beedrill", number: 15, description: " a Bug/Poison Type Pokémon with Rain/Cloudy Weather and a Rarity of 4. It is in its Third Evolutionary Stage with a Base Attack Level of 169\n\n"},
-    {name: "Mega-Beedrill", number: 15.1, description: " a Mega version of Beedrill with extra speed and rarity\n\n"},
-    {name: "Pidgey", number: 16, description: " a Normal/Flying Type Pokémon with Partly Cloudy/Windy Weather and a Rarity of 1. It is in its First Evolutionary Stage with a Base Attack Level of 85\n\n"},
-    {name: "Pidgeotto", number: 17, description: " a Normal/Flying Type Pokémon with Partly Cloudy/Windy Weather and a Rarity of 3. It is in its Second Evolutionary Stage with a Base Attack Level of 117\n\n"},
-    {name: "Pidgeot", number: 18, description: " a Normal/Flying Type Pokémon with Partly Cloudy/Windy Weather and a Rarity of 4. It is in its Third Evolutionary Stage with a Base Attack Level of 166\n\n"},
-    {name: "Mega-Pidgeot", number: 18.1, description: " a Mega version of Pidgeot with extra speed and rarity\n\n"},
-    {name: "Ratatta", number: 19, description: " a Normal Type Pokémon with Partly Cloudy Weather and a Rarity of 1. It is in its First Evolutionary Stage with a Base Attack Level of 103\n\n"},
-    {name: "Raticatte", number: 20, description: " a Normal Type Pokémon with Partly Cloudy Weather and a Rarity of 4. It is in its Second Evolutionary Stage with a Base Attack Level of 161\n\n"},
+    {name: "Bulbasaur", number: 1, description: " a Grass/Poison Type Pokémon with Clear/Cloudy Weather and a Rarity of 4. It is in its First Evolutionary Stage with a Base Attack Level of 118\n\n", img: new Image().src = 'pokemon/1.png'},
+    {name: "Ivysaur", number: 2, description: " a Grass/Poison Type Pokémon with Clear/Cloudy Weather and a Rarity of 5. It is in its Second Evolutionary Stage with a Base Attack Level of 151\n\n", img: new Image().src = 'pokemon/2.png'},
+    {name: "Venasaur", number: 3, description: " a Grass/Poison Type Pokémon with Clear/Cloudy Weather and a Rarity of 6. It is in its Third Evolutionary Stage with a Base Attack Level of 198\n\n", img: new Image().src = 'pokemon/3.png'},
+    {name: "Mega-Venasaur", number: 3.1, description: " the Mega version of Venasaur Type Pokémon with extra strength and rarity\n\n", img: new Image().src = 'pokemon/3-mega.png'},
+    {name: "Charmander", number: 4, description: " a Fire Type Pokémon with Clear Weather and a Rarity of 4. It is in its First Evolutionary Stage Type Pokémon with a Base Attack Level of 116\n\n", img: new Image().src = 'pokemon/4.png'},
+    {name: "Charmeleon", number: 5, description: " a Fire Type Pokémon with Clear Weather and a Rarity of 5. It is in its Second Evolutionary Stage Type Pokémon with a Base Attack Level of 158\n\n", img: new Image().src = 'pokemon/5.png'},
+    {name: "Charizard", number: 6, description: " a Fire/Flying Type Pokémon with Clear/Windy Weather and a Rarity of 6. It is in its Third Evolutionary Stage with a Base Attack Level of 223\n\n", img: new Image().src = 'pokemon/6.png'},
+    {name: "Mega-X-Charizard", number: 6.1, description: " a Mega version of Charizard Type Pokémon with extra strength and rarity\n\n", img: new Image().src = 'pokemon/6-mega-x.png'},
+    {name: "Mega-Y-Charizard", number: 6.2, description: " a Mega version of Charizard Type Pokémon with extra powers and rarity\n\n", img: new Image().src = 'pokemon/6-mega-y.png'},
+    {name: "Squirtle", number: 7, description: " a Water Type Pokémon with Rain Weather and a Rarity of 4. It is in its First Evolutionary Stage with a Base Attack Level of 94\n\n", img: new Image().src = 'pokemon/7.png'},
+    {name: "Wartortle", number: 8, description: " a Water Type Pokémon with Rain Weather and a Rarity of 5. It is in its Second Evolutionary Stage with a Base Attack Level of 126\n\n", img: new Image().src = 'pokemon/8.png'},
+    {name: "Blastoise", number: 9, description: " a Water Type Pokémon with Rain Weather and a Rarity of 6. It is in its Third Evolutionary Stage with a Base Attack Level of 171\n\n", img: new Image().src = 'pokemon/9.png'},
+    {name: "Mega-Blastoise", number: 9.1, description: " a Mega version of Blastoise with extra strength and rarity\n\n", img: new Image().src = 'pokemon/9-mega.png'},
+    {name: "Caterpie", number: 10, description: " a Bug Type Pokémon with Rain Weather and a Rarity of 1. It is in its First Evolutionary Stage with a Base Attack Level of 55\n\n", img: new Image().src = 'pokemon/10.png'},
+    {name: "Metapod", number: 11, description: " a Bug Type Pokémon with Rain Weather and a Rarity of 3. It is in its Second Evolutionary Stage with a Base Attack Level of 45\n\n", img: new Image().src = 'pokemon/11.png'},
+    {name: "Butterfree", number: 12, description: " a Bug/Flying Type Pokémon with Rain/Windy Weather and a Rarity of 4. It is in its Third Evolutionary Stage with a Base Attack Level of 167\n\n", img: new Image().src = 'pokemon/12.png'},
+    {name: "Weedle", number: 13, description: " a Bug/Poison Type Pokémon with Rain/Cloudy Weather and a Rarity of 1. It is in its First Evolutionary Stage with a Base Attack Level of 63\n\n", img: new Image().src = 'pokemon/13.png'},
+    {name: "Kakuna", number: 14, description: " a Bug/Poison Type Pokémon with Rain/Cloudy Weather and a Rarity of 3. It is in its Second Evolutionary Stage with a Base Attack Level of 46\n\n", img: new Image().src = 'pokemon/14.png'},
+    {name: "Beedrill", number: 15, description: " a Bug/Poison Type Pokémon with Rain/Cloudy Weather and a Rarity of 4. It is in its Third Evolutionary Stage with a Base Attack Level of 169\n\n", img: new Image().src = 'pokemon/15.png'},
+    {name: "Mega-Beedrill", number: 15.1, description: " a Mega version of Beedrill with extra speed and rarity\n\n", img: new Image().src = 'pokemon/15-mega.png'},
+    {name: "Pidgey", number: 16, description: " a Normal/Flying Type Pokémon with Partly Cloudy/Windy Weather and a Rarity of 1. It is in its First Evolutionary Stage with a Base Attack Level of 85\n\n", img: new Image().src = 'pokemon/16.png'},
+    {name: "Pidgeotto", number: 17, description: " a Normal/Flying Type Pokémon with Partly Cloudy/Windy Weather and a Rarity of 3. It is in its Second Evolutionary Stage with a Base Attack Level of 117\n\n", img: new Image().src = 'pokemon/17.png'},
+    {name: "Pidgeot", number: 18, description: " a Normal/Flying Type Pokémon with Partly Cloudy/Windy Weather and a Rarity of 4. It is in its Third Evolutionary Stage with a Base Attack Level of 166\n\n", img: new Image().src = 'pokemon/18.png'},
+    {name: "Mega-Pidgeot", number: 18.1, description: " a Mega version of Pidgeot with extra speed and rarity\n\n", img: new Image().src = 'pokemon/18-mega.png'},
+    {name: "Ratatta", number: 19, description: " a Normal Type Pokémon with Partly Cloudy Weather and a Rarity of 1. It is in its First Evolutionary Stage with a Base Attack Level of 103\n\n", img: new Image().src = 'pokemon/19.png'},
+    {name: "Raticatte", number: 20, description: " a Normal Type Pokémon with Partly Cloudy Weather and a Rarity of 4. It is in its Second Evolutionary Stage with a Base Attack Level of 161\n\n", img: new Image().src = 'pokemon/20.png'},
 ]
 
 // to validate input of the String Search Bar
@@ -42,57 +42,61 @@ function validateName(){
     }
 }
 
-// check if the enter key was pressed on the number input
-function entered(){
-  var input = document.getElementById('num-input');
-  input.addEventListener("keydown", function(e){
-    e.stopImmediatePropagation(); // prevents the event from being called too many times
-      if(e.key === "Enter"){
-        e.preventDefault();
-        byNumber();
-      }
-      if(e.key === "e"){
-        e.preventDefault();
-      }
-  });
-}
+// // check if the enter key was pressed on the number input
+// function entered(){
+//   var input = document.getElementById('num-input');
+//   input.addEventListener("keydown", function(e){
+//     e.stopImmediatePropagation(); // prevents the event from being called too many times
+//       if(e.key === "Enter"){
+//         e.preventDefault();
+//         byNumber();
+//       }
+//       if(e.key === "e"){
+//         e.preventDefault();
+//       }
+//   });
+// }
 
-// check if the enter key was pressed on the name input
-function entered2(){
-  var input2 = document.getElementById('name-input');
-  input2.addEventListener("keydown", function(e){
-    e.stopImmediatePropagation();
-      if(e.key === "Enter"){
-        flag2 = true;
-        e.preventDefault();
-        byName();
-      }
-  });
-}
+// // check if the enter key was pressed on the name input
+// function entered2(){
+//   var input2 = document.getElementById('name-input');
+//   input2.addEventListener("keydown", function(e){
+//     e.stopImmediatePropagation();
+//       if(e.key === "Enter"){
+//         flag2 = true;
+//         e.preventDefault();
+//         byName();
+//       }
+//   });
+// }
 
 
 function byNumber(){
     let numInput = document.getElementById('num-input').value;               // so the comparison is not case-senstive
-    let ul = document.getElementById("pokemon-list");            
+    let ul = document.getElementById("pokemon-list");   
+    
+    let newUl = document.getElementById("results-list");
+
     let p = ul.getElementsByTagName("p");  
     let counter = 0;        // to keep track of how many matches are added  
     let matches = [];    
     let inp = numInput.replace(/^0+/, '');    // to ignore leading zeros
     // go through number list and see if the values match the input                
     for (i = 0; i < p.length; i++) {
-        value = p[i].textContent || p[i].innerText; 
-
-        // check if the input matches the inner text or text content
+        value = p[i].textContent;    
+        // check if the input matches the inner tex t or text content
       if (value.indexOf(inp) > -1 && counter < 5) {
         counter ++;
         // only show the list value if it matches the input
-        matches[i] = `#${pokemon[i].number}. ${pokemon[i].name}${pokemon[i].description}`;
+        matches[i] = pokemon[i].number;
       } 
     }
     // print the array if there was at least one match
-    // if(matches.length != 0){
-    //   alert(matches.join(''));
-    // }
+    if(matches.length != 0){
+      alert(matches.join(''));
+    }
+
+    // displayResults(matches);
 }
  
 // Function to search by name
@@ -115,13 +119,14 @@ function byName() {
     }
     else if (value.toLowerCase().indexOf(sameCase) > -1 && counter < 5) {
       counter ++;
-      matches[i] = `#${pokemon[i].number}. ${pokemon[i].name}:${pokemon[i].description}`;
+      matches[i] = `#${pokemon[i].number}. ${pokemon[i].name}:${pokemon[i].description}`;      
     } 
   }
   // print the array if there was at least one match
-  // if(matches.length != 0){
-  //   alert(matches.join(''));
-  // }else{
+  if(matches.length != 0){
+    displayResults(matches);
+  }
+  // else{
   //   alert("Please try again");
   // }
 }
@@ -134,3 +139,24 @@ function byName() {
       // create a copy of that elemtent in the search results box up top
 // and then adding the newly created elements to the selected node using appendChild() method.
       // add newly created elemnts to the new div in a list
+
+function displayResults(array){
+  // array.forEach(function(e){
+  //   // if the array name matches an id from the html displayed
+
+  //   // and add that list element to the other list
+
+  //   // display the other list at all time
+
+  //   var li = document.createElement('li');
+  //   var text = document.createTextNode(`${array[4]}`)
+  //   li.appendChild(text);
+  //   var ul = document.querySelector('#results-list')
+  //   ul.appendChild(li);
+  //   console.log(li);
+  // })
+}
+
+
+// JUST ADD THINGS FROM THE ARRAY
+// LIKE ADD THE IMAGE AND THE APAN AND THE TITLE AND STUFF
